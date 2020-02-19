6e2d217300448eec89a415e0e0d0d5bfba480767
EC2 Default User <ec2-user@ip-10-0-139-213.ec2.internal>
2020-02-19 11:19:41 +0000

    hhhh

--- deploy/dns/createdns.sh
+++ deploy/dns/createdns.sh
@@ -1,4 +1,4 @@
 sleep 30
-LBNAME=`/usr/local/bin/kubectl describe service frontend|grep Ingress|awk '{print $NF}'`
+LBNAME=`/usr/local/bin/kubectl describe service api|grep Ingress|awk '{print $NF}'`
 sed -i "s/FELBNAME/$LBNAME/g" dns.json
-aws route53 change-resource-record-sets --hosted-zone-id Z1FZZENY46SZHD --change-batch file://dns.json
+aws route53 change-resource-record-sets --hosted-zone-id Z1H1FL5HABSF5 --change-batch file://dns.json
